e785d726cd7f6511a927295a9bf2d88d38c7cb33
SivaNadupuri <siva@DESKTOP-VI5U1O2>
2020-05-12 20:41:11 +0530

    order service initiated

---
+++ order-service/src/test/java/com/myvillage/rder/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myvillage.rder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
